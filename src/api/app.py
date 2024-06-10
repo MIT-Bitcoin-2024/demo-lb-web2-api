62c3ad44c2cca1462a1b12c4d04db6571db03b77
Pavel Kononov <kononoffp@gmail.com>
2024-06-10 04:13:04 -0400

    CORS fix

--- src/api/app.py
+++ src/api/app.py
@@ -1,9 +1,19 @@
 from fastapi import FastAPI, status
+from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import RedirectResponse
 
 from .routers import api
 
+
 app = FastAPI(title='Lightning Bounties Demo')
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=['*'],
+)
 
 app.include_router(api.router, prefix='/api')
 
